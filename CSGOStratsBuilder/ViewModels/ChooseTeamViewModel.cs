f74003500b6950ce3bd0909fc61ff263b5c2c563
Maxime <maxime.ferreira@viacesi.fr>
2021-11-20 16:39:41 +0100

    Update to use xml for team file

--- CSGOStratsBuilder/ViewModels/ChooseTeamViewModel.cs
+++ CSGOStratsBuilder/ViewModels/ChooseTeamViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CSGOStratsBuilder.Model.Commands;
 using CSGOStratsBuilder.Model.Services;
 using CSGOStratsBuilder.Model.Stores;
+using CSGOStratsBuilder.Model.UseCase;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
@@ -11,20 +12,29 @@ namespace CSGOStratsBuilder.ViewModels {
 
         private readonly ObservableCollection<TeamViewModel> _team;
 
+        private CreateTeamFile createTeamFile = new CreateTeamFile();
+        private ReadTeamFile readTeamFile = new ReadTeamFile();
+
         public IEnumerable<TeamViewModel> Team => _team;
         public ICommand AddTeamCommand { get; }
         public ICommand NavigateConfigCommand { get; }
 
         public ChooseTeamViewModel(TeamStore teamStore, INavigationService addTeamNavigationService, INavigationService configNavigationService) {
+            List<string> teamsAlreadyAdded = readTeamFile.Execute();
+
             _teamStore = teamStore;
             AddTeamCommand = new NavigateCommand(addTeamNavigationService);
             NavigateConfigCommand = new NavigateCommand(configNavigationService);
             _team = new ObservableCollection<TeamViewModel>();
+            foreach(string team in teamsAlreadyAdded) {
+                _team.Add(new TeamViewModel(team));
+            }
             _teamStore.TeamAdded += OnTeamAdded;
         }
 
         private void OnTeamAdded(string name) {
             _team.Add(new TeamViewModel(name));
+            createTeamFile.Execute(name);
         }
     }
 }
